995b2cc2af82dbca959b1713ffcbc1e491d7d4cf
Akinbode Weezykon <Weezykon@gmail.com>
2022-02-23 09:56:31 +0100

    error handler

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
+    const statusCode = res.statusCode || 500;
     const message = err.message || 'Something went wrong';
     const stack = process.env.NODE_ENV === 'production' ? 'Something went wrong' : err.stack;
     res.status(statusCode).json({
